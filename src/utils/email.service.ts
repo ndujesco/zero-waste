f3189feadf43a4f30667795f994462f35413a55c
ugo <ndujesco@users.noreply.github.com>
2023-07-26 09:00:57 +0100

    update

--- src/utils/email.service.ts
+++ src/utils/email.service.ts
@@ -28,13 +28,13 @@ export class EmailService {
           <div style="border-bottom:1px solid #eee">
               <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Zero Waste</a>
           </div>
-          <p style="font-size:1.1em">Hi, ${username}</p>
+          <p style="font-size:1.1em">Hi, ${username}.</p>
           <p>Thank you for choosing Zero-Waste. Use the following OTP to complete your Sign Up procedures. OTP is valid
-              for 5 minutes</p>
+              for 5 minutes.</p>
           <h2
               style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
               ${otp}</h2>
-          <p style="font-size:0.9em;">Regards,<br />Zero Waste</p>
+          <p style="font-size:0.9em;">Regards,<br />Zero Waste.</p>
           <hr style="border:none;border-top:1px solid #eee" />
           <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
           </div>
