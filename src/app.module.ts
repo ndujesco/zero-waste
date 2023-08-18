eeb892102eea34ebe97f86938d52cb444243acff
ugo <ndujesco@users.noreply.github.com>
2023-08-18 12:38:18 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './user/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UtilsModule } from './utils/utils.module';
 import { MessagesModule } from './messages/messages.module';
+import { FileUploadModule } from './file_upload/file_upload.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, UtilsModule, MessagesModule],
+  imports: [UsersModule, PrismaModule, UtilsModule, MessagesModule, FileUploadModule],
 })
 export class AppModule {}
