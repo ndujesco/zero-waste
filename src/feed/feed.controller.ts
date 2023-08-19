f4c257cac91c6ad645cfe1ba1ff5d86c26100006
ugo <ndujesco@users.noreply.github.com>
2023-08-19 01:18:46 +0100

    update

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FeedService } from './feed.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Controller('feed')
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      preservePath: true,
+      storage: memoryStorage(),
+    }),
+  )
+  async uploadToFeed(@UploadedFiles() file: Express.Multer.File) {
+    return;
+  }
 }
